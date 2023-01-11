e6725413d8143b0312c2fdbccdbf3c12b8cc6877
Muhammad Hamza <48496170+hamzayousuf121@users.noreply.github.com>
2023-01-11 16:13:24 +0500

    migraion

---
+++ Migrations/20230111111252_add-fields.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication2.Migrations
+{
+    /// <inheritdoc />
+    public partial class addfields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "PatientPhoneNo",
+                table: "Appointments",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PatientAge",
+                table: "Appointments",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<int>(
+                name: "AppointmentStatusId",
+                table: "Appointments",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Appointments_AppointmentStatusId",
+                table: "Appointments",
+                column: "AppointmentStatusId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Appointments_AppointmentStatuses_AppointmentStatusId",
+                table: "Appointments",
+                column: "AppointmentStatusId",
+                principalTable: "AppointmentStatuses",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Appointments_AppointmentStatuses_AppointmentStatusId",
+                table: "Appointments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Appointments_AppointmentStatusId",
+                table: "Appointments");
+
+            migrationBuilder.DropColumn(
+                name: "AppointmentStatusId",
+                table: "Appointments");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PatientPhoneNo",
+                table: "Appointments",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PatientAge",
+                table: "Appointments",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
